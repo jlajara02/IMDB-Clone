bf49b5bb67594eae4db2403c391ce3e76652095a
Juan Lajara <juan@teamraderie.com>
2024-03-26 21:49:23 -0400

    show data without component "li"

--- src/app/page.js
+++ src/app/page.js
@@ -12,11 +12,12 @@ function Home() {
   const searchParams = useSearchParams();
   const genre = searchParams.get('genre') || 'fetchTrending';
   console.log("process.env.IMDB_API_KEY: " + process.env.IMDB_API_KEY )
-  console.log("IMDBAPI: " + IMDBAPI )
-  console.log("genre: " + genre )
+  console.log("IMDBAPI: " + IMDBAPI );
+  console.log("genre: " + genre );
+  
   //const genre = 'fetchTrending';
   const url = `https://api.themoviedb.org/3${genre === 'fetchTopRated' ? `/movie/top_rated`:`/trending/all/week`}?api_key=${IMDBAPI}&language=en-US`
-  console.log("url: " + url )
+  console.log("url :" + url);
   
   const [movieData, setMovieData] = useState(0)
   useEffect (() =>{
@@ -38,27 +39,18 @@ function Home() {
   //let dataJSON = (JSON.stringify(movieData));
   console.log("dataJSON :" + dataJSON);
 
-  //let dataArray = [];// Array.from(dataJSON)
+  let dataArray = Array.from(dataJSON)
   //console.log("typeof: " + typeof(dataArray));
   
   //console.log("dataArray :" + dataArray);
 
-  /* let userElements = dataArray.map(function(dataJSON) {
+  let userElements = dataArray.map(function(dataJSON) {
+    //console.log("dataArray dataJSON :" + dataJSON.name);
     //let obj =  JSON.stringify(dataArray);
-    return dataJSON;
-  });
-   */
+    return <li key={dataJSON.id}> {dataJSON.name} </li>
+  }); 
 
-  console.log("userElements :" + dataJSON);
-
-  for (const [key, value] of Object.entries(dataJSON)) {
-    //console.log(`Key: ${key}, Value: ${value}`)
-    for (const [key1, value1] of Object.entries(value)) {
-      console.log(`Key: ${key1}, Value: ${value1}`)
-      
-    } 
-    
-  } 
+  console.log("userElements :" + userElements);
 
   return (
     <div> home page <br/>
@@ -70,7 +62,7 @@ function Home() {
       } */
       
       //userElements.forEach((element) => {element.title})
-      //userElements
+      userElements
     } </ul></div>
   )
 }
